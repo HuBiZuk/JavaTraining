5498a1bb2880ce985ca78d6d91c2741dc15ed369
sungminHA <nicesm0407@gmail.com>
2025-10-23 17:24:24 +0900

    2510231724

---
+++ JAVA/src/Chapter05_배열/Ex05_12_ArrayEx12.java
@@ -0,0 +1,14 @@
+package Chapter05_배열;
+
+import java.util.Arrays;
+
+public class Ex05_12_ArrayEx12 {
+	public static void main(String[] args) {
+		String[] names = {"kim", "Park", "Yi"};
+		
+		for(int i=0; i<names.length; i++) {
+			
+		}
+		
+	}	
+}
